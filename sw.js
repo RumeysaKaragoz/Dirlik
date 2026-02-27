557d69108c66991fd40d17dbbf0219478be25af5
Rümeysa Karagöz <rmeysakaragz0@gmail.com>
2026-02-27 18:01:21 +0300

    Refactor service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,31 +1,20 @@
-const CACHE_NAME = "deprem-yardim-v1";
-
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/firstaid.html",
-  "/style.css",
-  "/script.js",
-   "/audio.js",
-  "/audio/kanama.mp3",
-  "/audio/enkaz.mp3",
-  "/audio/bilinc.mp3"
+const CACHE_NAME = 'deprem-yardim-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/firstaid.html'
 ];
 
-
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
